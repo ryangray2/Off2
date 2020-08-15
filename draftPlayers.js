0e6c4d372078c85ed94f6271b396f546160f9c85
ryangray2 <ryangray@udel.edu>
2020-08-15 19:51:22 -0400

    Update draftPlayers.js

--- draftPlayers.js
+++ draftPlayers.js
@@ -5,7 +5,8 @@ var wiseman = {
 	school: "Memphis",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 2.3,
+	tkw: "I’m not saying the Knicks have to set their draft board on fire and trade up to draft James Wiseman with the first overall pick. Rather, if New York finds themselves in a Joel Embiid/76ers situation where Wiseman falls into their lap with a later pick, they shouldn’t pass on him due to roster construct. Worst case scenario, they can trade either Wiseman or Robinson down the line for draft picks or a player who’s a better fit for their roster. - Tim Kohut",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4432808.png&w=350&h=254"
 };
 
@@ -16,7 +17,8 @@ var edwards = {
 	school: "Georgia",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 1.3,
+	tkw: "Edwards is the Barrett of the 2020 class in the sense that his weaknesses and lowlights have been screamed and his strengths have been whispered. He is an unreal athlete with a mechanically smooth shot. - Mike Cortez",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4594268.png&w=350&h=254"
 };
 
@@ -27,7 +29,8 @@ var ball = {
 	school: "Illawarra",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 2.1,
+	tkw: "LaMelo Ball is the franchise-altering point guard that the Knicks need. He’s a player who will make everyone on the team better with his lights-out passing, and has a chance to be a multiple-time All-Star if he can become more efficient and shoot better. The shooting isn’t much of a concern to me due to the fact that it’s only a 12-game sample size, and Ball also shot 72.3% from the free throw line. - Dylan Burd",
 	headshot: "ball.png"
 };
 
@@ -38,7 +41,8 @@ var avdija = {
 	school: "Maccabi Tel Aviv",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 3.5,
+	tkw: "If nothing else, Deni Avdija is one of the more versatile prospects in this year’s class. If he is able to correct the shot and become a legitimate shooting threat, there is no telling what he will be able to accomplish in the NBA. If he is not, he will still be a playmaker who can score inside and find his teammates for easy looks offensively, as well as provide sound team defense with good help side defense and rim protection. - Nick Carannante",
 	headshot: "deni.png"
 };
 
@@ -49,7 +53,8 @@ var toppin = {
 	school: "Dayton",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 5,
+	tkw: "Toppin’s pro outlook hinges on his ability to shoot the basketball, and as TKW’s Nick Carannante mentioned in his Toppin profile, his strong shot selection can prevent a Josh Smith 2.0 scenario from happening and compared Toppin’s potential fit in New York with Amar’e Stoudemire. - Mike Cortez",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4278355.png&w=350&h=254"
 };
 
@@ -60,7 +65,8 @@ var hayes = {
 	school: "Ratiopharm Ulm",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 1.3,
+	tkw: "The foundation of scoring potential, willingness to defend, and high-level playmaking are plain to see. If given a year or two to hone his skills, Hayes could end up being the best player in this draft. - Mike Cortez",
 	headshot: "killian.png"
 };
 
@@ -71,7 +77,8 @@ var haliburton = {
 	school: "Iowa State",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 2.2,
+	tkw: "In many ways, an apt comparison for Haliburton may be Malcolm Brogdon. Brogdon was a second-round pick out of Virginia, but the maturity and feel were unquestioned. Brogdon outpaced a weak first-year class to win Rookie of the Year and is now a borderline All-Star from just constantly making the right decision and being a superb teammate. Ideally, Haliburton’s traits could translate in the same manner. - Michael Corvo",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4396993.png&w=350&h=254"
 };
 
@@ -82,7 +89,7 @@ var okongwu = {
 	school: "USC",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 3.6,
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4431680.png&w=350&h=254"
 };
 
@@ -93,7 +100,8 @@ var okoro = {
 	school: "Auburn",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 2.4,
+	tkw: "Here are the career free throw percentages of Okoro, Barrett, and Robinson: 67.4% (NCAA only), 61.4%, and 56.8%, respectively. In a game that is only becoming more about spacing, this is a remarkably daunting outlook. Now, all three would be incredibly young, and there would be time for them to grow and improve—but the organization has never been known for its patience. - Brendan Campbell",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4432822.png&w=350&h=254"
 };
 
@@ -104,7 +112,7 @@ var anthony = {
 	school: "North Carolina",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: .3,
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4432809.png&w=350&h=254"
 };
 
@@ -115,7 +123,8 @@ var vassell = {
 	school: "Florida State",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 2.7,
+	tkw: "What makes (Vassell) a lottery pick is his ability to affect both sides of the floor. Offensively, Vassell was elite at the rim and from beyond the arc, finishing 69.4% at the rim and 41.2% from three-point range. Combine that with 3.1 stocks (steals plus blocks) and you have a pretty high-level wing prospect at 19 years old. - Quentin Haynes",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4395630.png&w=350&h=254"
 };
 
@@ -126,7 +135,7 @@ var nesmith = {
 	school: "Vanderbilt",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 1.5,
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4396909.png&w=350&h=254"
 };
 
@@ -137,7 +146,8 @@ var bey = {
 	school: "Villanova",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: .9,
+	tkw: "Bey is exactly the type of shooter that can disseminate a crowded floor and unclog driving lanes for Barrett, Julius Randle, and other drivers. His defense would be an added bonus. - Mike Cortez",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4397136.png&w=350&h=254"
 };
 
@@ -149,6 +159,7 @@ var hampton = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "Hampton is a pretty decent boom or bust candidate in my opinion. He could become a really interesting player, but I doubt New York is where it happens. While you don’t want to draft for fit in the top 10, Hampton is probably the worst fit for this roster of any guard projected in the lottery, and is unlikely to be considered the best player available even if the Knicks fall to nine. - Eli Cohen",
 	headshot: "hampton.png"
 };
 
@@ -159,7 +170,7 @@ var maxey = {
 	school: "Kentucky",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 3.4,
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4431678.png&w=350&h=254"
 };
 
@@ -204,6 +215,7 @@ var lewisJr = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "Kira Lewis Jr. has room for improvement in terms of handles and boosting his physique, but the latter will inevitably influence how much better he is on the defensive end. Long term, Lewis Jr.’s speed and basketball I.Q. will complete a trio (Barrett and Robinson) that brings back the run-and-gun New York basketball that many fans romanticize. - James Woodruff",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4395698.png&w=350&h=254"
 };
 
@@ -214,7 +226,7 @@ var ramsey = {
 	school: "Texas Tech",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: .9,
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4431688.png&w=350&h=254"
 };
 
@@ -226,6 +238,7 @@ var mannion = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "If he drops to 27th, it might not be a bad idea to take him. With a great feel for the game as a facilitator and a tough mindset, Mannion has a high floor, albeit a limited ceiling. New York has had a revolving door of point guards in recent years, and he might add some stability as a backup that looks to create for others. - Eugene Rapay",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4431672.png&w=350&h=254"
 };
 
@@ -236,7 +249,8 @@ var terry = {
 	school: "Stanford",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 1.1,
+	tkw: "Most teams should have Terry pegged as a high-end bench player, some maybe even higher than that. With a smooth jumper like his, it’s hard not to buy into him being an effective pro; but as TKW’s Eli Cohen said, he has room for improvement as a floor general and defending one-on-one. - Mike Cortez",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4431747.png&w=350&h=254"
 };
 
@@ -247,7 +261,7 @@ var maledon = {
 	school: "ASVEL",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: 2.1,
 	headshot: "maledon.png"
 };
 
@@ -259,6 +273,7 @@ var smith = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "For the Knicks, they should be wary of taking a raw, developmental prospect like Smith. New York is a team that just needs talent right now, and the way to do that in the late first round, early second round is drafting players with higher floors than Jalen Smith. - Dylan Burd",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4397189.png&w=350&h=254"
 };
 
@@ -302,7 +317,7 @@ var aleksej = {
 	school: "Olympiacos B",
 	image: "",
 	cSalary: 0,
-	wa: 0,
+	wa: .8,
 	headshot: "aleksej.png"
 };
 
@@ -314,6 +329,7 @@ var jones = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "Stephen and Seth Curry. Marcus and Markieff Morris. The Plumlees. The Holidays. It’s often the eldest brother that creates a lofty ideal of what his younger brother could be. Tyus Jones, thus far, has been mostly unremarkable. However, Tre Jones is better than his brother, particularly as a passer and a defender. Spencer Pearlman of The Stepien projects Tre Jones to be a backup guard. I’m inclined to agree with this assessment. - James Woodruff",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4395626.png&w=350&h=254"
 };
 
@@ -349,6 +365,7 @@ var bane = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "The numbers of intrigue: 6.5 three-point attempts per game and 44.2% three-point shooting. Not the biggest wing at 6-foot-5 and already 22 years old, there are concerns, but Bane has the numbers of a solid option after the first 20 picks. - Quentin Haynes",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4066320.png&w=350&h=254"
 };
 
@@ -371,6 +388,7 @@ var reed = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "Everything about Reed’s positives points to the notion that Thibodeau would love his game on the defensive end. He’s physically gifted, he has some good instincts on defense, and he still has room to grow into his body. Because he can pick the passing lane, Reed is a nice addition in the second unit to give those Knicks’ runs some breathing room. - James Woodruff",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4278562.png&w=350&h=254"
 };
 
@@ -387,12 +405,13 @@ var careyJr = {
 
 var woodardII = {
 	firstName: "Robert",
-	lastName: "Woodard II",
+	lastName: "Woodard",
 	position: "SF",
 	school: "Mississippi State",
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "At 20 years old, Woodard II is the exact type of player that Tom Thibodeau can develop into a serviceable All-NBA defender. Sporting an intimidating wingspan, Woodard has the body to be an utter nuisance at all five positions. His length can be used to tighten up the perimeter defense or he can bolster the second unit’s interior defense as another rim protector. - James Woodruff",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4396961.png&w=350&h=254"
 };
 
@@ -448,6 +467,7 @@ var tillie = {
 	image: "",
 	cSalary: 0,
 	wa: 0,
+	tkw: "The caveat with Tillie is obviously his health. Guys who carry the injury-prone stigma have to fight against those worries. It’s especially rough when those injuries involve the knees and ankles. Time missed aside, Tillie is one of the most consistent big men in the NCAA offensively. He’s shown enough improvements on defense that I believe he’d make a great project for Thibodeau. Tillie’s ideal tier is becoming a Nikola Mirotic type of player. - James Woodruff",
 	headshot: "https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/4066651.png&w=350&h=254"
 };
 
